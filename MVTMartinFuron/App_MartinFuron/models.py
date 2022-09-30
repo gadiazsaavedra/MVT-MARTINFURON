a361af3b7be39fcfe582df04741354fb6466f90b
Martín Ignacio Furon <martinfuron@gmail.com>
2022-09-30 20:05:00 -0300

    primer modelo

--- MVTMartinFuron/App_MartinFuron/models.py
+++ MVTMartinFuron/App_MartinFuron/models.py
@@ -1,3 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+class   familia(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    telefono = models.CharField(max_length=40)
+    email = models.EmailField()
+
+
